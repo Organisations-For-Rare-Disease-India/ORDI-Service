25af2d5af36a6af31620e8aeb5da260833ccf478
Vedanta Jha <jhavedanta@hotmail.com>
2024-09-14 17:56:11 +0100

    Stick to HTTP, handling HTTPS with nginx

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,22 +3,12 @@ package main
 import (
 	"ORDI/internal/server"
 	"fmt"
-	"os"
 )
 
 func main() {
 
 	server := server.NewServer()
-	certFile := os.Getenv("SSL_CERT_PATH")
-	keyFile := os.Getenv("SSL_KEY_PATH")
-	isLocal := os.Getenv("IS_LOCAL") == "true"
-	var err error
-	if isLocal {
-		err = server.ListenAndServe()
-	} else {
-		server.Addr = ":443"
-		err = server.ListenAndServeTLS(certFile, keyFile)
-	}
+	err := server.ListenAndServe()
 	if err != nil {
 		panic(fmt.Sprintf("cannot start server: %s", err))
 	}
