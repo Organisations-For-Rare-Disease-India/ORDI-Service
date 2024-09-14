7374d4f6ce1f10771244c274e21a27ba18df9fea
Vedanta Jha <jhavedanta@hotmail.com>
2024-09-14 16:54:21 +0100

    Server HTTPS

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"ORDI/internal/server"
 	"fmt"
+	"os"
 )
 
 func main() {
 
 	server := server.NewServer()
-
-	err := server.ListenAndServe()
+	certFile := os.Getenv("SSL_CERT_PATH")
+	keyFile := os.Getenv("SSL_KEY_PATH")
+	isLocal := os.Getenv("IS_LOCAL") == "true"
+	var err error
+	if isLocal {
+		err = server.ListenAndServe()
+	} else {
+		server.Addr = ":443"
+		err = server.ListenAndServeTLS(certFile, keyFile)
+	}
 	if err != nil {
 		panic(fmt.Sprintf("cannot start server: %s", err))
 	}
